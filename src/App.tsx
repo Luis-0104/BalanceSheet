6b9ff622d49e3c8f8a36f2fde113912ba1f3f081
luis-0104 <schweinberger@milongroup.com>
2022-10-27 14:47:16 +0200

    addButton

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
-      <h1></h1>
+      <h1>BalanceSheet</h1>
+      <div>
+        <button id ="addButton">+</button>
+      </div>
     </div>
   )
 }
