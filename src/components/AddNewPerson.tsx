096d0738c8dd11d1b400219e8349bb90cfcc7846
luis-0104 <schweinberger@milongroup.com>
2022-11-03 12:55:19 +0100

    inputCheck works aber Pfusch

--- src/components/AddNewPerson.tsx
+++ src/components/AddNewPerson.tsx
@@ -14,11 +14,9 @@ export function AddNewPerson({ showAddMenu, toggleShowAddMenu }: AddNewPersonPro
     const [balanceInput, setBalanceInput] = useState(0)
     const [inputHint, setInputHint] = useState('')
 
-    function checkInput(): boolean {
-
-
+    function checkNameInput() {
         //check if name is empty
-        if(nameInput==''){
+        if (nameInput == '') {
             setInputHint(`Name cannot be empty!`)
             return false;
         }
@@ -31,9 +29,19 @@ export function AddNewPerson({ showAddMenu, toggleShowAddMenu }: AddNewPersonPro
             }
         }
 
+        if(inputHint.includes('Name cannot be empty!')||inputHint.includes('is already taken!')){
+            setInputHint('')
+            checkBalanceInput();
+            checkEmailInput();
+        }
+        return true;
 
+    }
+
+
+    function checkEmailInput() {
         // check if email is empty
-        if(emailInput==''){
+        if (emailInput == '') {
             setInputHint(`The Email-Adress cannot be empty!`)
             return false;
         }
@@ -44,21 +52,47 @@ export function AddNewPerson({ showAddMenu, toggleShowAddMenu }: AddNewPersonPro
             .match(
                 /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             )) {
-            setInputHint(`\'${emailInput} isn't a valid email-adress!`);
+            setInputHint(`${emailInput} isn't a valid email-adress!`);
             return false;
         }
 
+        if(inputHint.includes('isn\'t a valid email-adress')){
+            setInputHint('')
+            checkBalanceInput();
+            checkNameInput();
+        }
+
+        return true;
+    }
+
+    function checkBalanceInput() {
         // check if startingBalance is valid
         if (balanceInput > 100 || balanceInput < 0) {
             setInputHint(`${balanceInput}€ isn't a valid starting balance. The starting balance has to be between 0€ and 100€!`);
             return false;
         }
+        if(inputHint.includes(`isn't a valid starting balance. The starting balance has to be between 0€ and 100€!`)){
+            setInputHint('')
+            checkEmailInput();
+            checkNameInput();
+        }
 
-        setInputHint('')
+        
         return true;
+
     }
 
+    function checkInput(): boolean {
+        if ((checkBalanceInput() && checkNameInput() && checkEmailInput())) {
+            setInputHint('');
+            return true;
+        }
+        return false;
+    }
 
+    function addPerson() { 
+        checkInput();
+    }
 
     if (!showAddMenu) {
         return <></>
@@ -66,11 +100,11 @@ export function AddNewPerson({ showAddMenu, toggleShowAddMenu }: AddNewPersonPro
 
 
     return <div id="AddNewPersonContainer">
-        <input placeholder="Name" id="nameInput" onChange={(evt) => setNameInput(evt.target.value)} onBlur={(evt)=>checkInput()}></input>
-        <input placeholder="email" id="emailInput" type="email" onChange={(evt) => setEmailInput(evt.target.value)} onBlur={(evt)=>checkInput()}></input>
-        <input placeholder="Starting Balance" id="balanceInput" type="number" min="0" step="any" onChange={(evt) => setBalanceInput(+evt.target.value)} onBlur={(evt)=>checkInput()}></input>
+        <input placeholder="Name" id="nameInput" onChange={(evt) => setNameInput(evt.target.value)} onBlur={(evt) => { checkNameInput(); }}></input>
+        <input placeholder="email" id="emailInput" type="email" onChange={(evt) => setEmailInput(evt.target.value)} onBlur={(evt) => { checkEmailInput(); }}></input>
+        <input placeholder="Starting Balance" id="balanceInput" type="number" min="0" step="any" onChange={(evt) => setBalanceInput(+evt.target.value)} onBlur={(evt) => { checkBalanceInput(); }}></input>
         €
-        <button id="submitAddButton" onClick={(evt) => { console.log(checkInput()) }}>Add</button>
+        <button id="submitAddButton" onClick={(evt) => { addPerson() }}>Add</button>
         <div>{inputHint}</div>
     </div>
 }
