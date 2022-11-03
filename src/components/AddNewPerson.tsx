8d2d4d836a12a27478d5799c4ea90c648f9793ff
luis-0104 <schweinberger@milongroup.com>
2022-11-03 11:48:53 +0100

    implemented checkinput()

--- src/components/AddNewPerson.tsx
+++ src/components/AddNewPerson.tsx
@@ -1,3 +1,4 @@
+import { ReactNode, useState } from 'react';
 import data from '../data/data.json'
 
 type AddNewPersonProps = {
@@ -8,44 +9,68 @@ type AddNewPersonProps = {
 
 export function AddNewPerson({ showAddMenu, toggleShowAddMenu }: AddNewPersonProps) {
 
-    var nameInput: string = '';
-    var emailInput: string = '';
-    var balanceInput: number = 0;
+    const [nameInput, setNameInput] = useState('')
+    const [emailInput, setEmailInput] = useState('')
+    const [balanceInput, setBalanceInput] = useState(0)
+    const [inputHint, setInputHint] = useState('')
 
     function checkInput(): boolean {
 
-        // check if name is already taken:
-        data.map((element) => {
-            if(element.first_name == nameInput){
-                console.log(`Your chosen name: ${nameInput} is already taken`)
-                return false;
-            }
-        })
 
-        // check if email is valid
-        if(!String(emailInput)
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        )){
-            console.log(`${emailInput} isn't a valid email-adress`)
+        //check if name is empty
+        if(nameInput==''){
+            setInputHint(`Name cannot be empty!`)
             return false;
         }
 
+        // check if name is taken
+        for (let element of data) {
+            if (element.first_name == nameInput) {
+                setInputHint(`Your chosen name: ${nameInput} is already taken!`);
+                return false;
+            }
+        }
 
+
+        // check if email is empty
+        if(emailInput==''){
+            setInputHint(`The Email-Adress cannot be empty!`)
+            return false;
+        }
+
+        // check if email is valid
+        if (!String(emailInput)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )) {
+            setInputHint(`\'${emailInput} isn't a valid email-adress!`);
+            return false;
+        }
+
+        // check if startingBalance is valid
+        if (balanceInput > 100 || balanceInput < 0) {
+            setInputHint(`${balanceInput}€ isn't a valid starting balance. The starting balance has to be between 0€ and 100€!`);
+            return false;
+        }
+
+        setInputHint('')
         return true;
     }
+
 
 
     if (!showAddMenu) {
         return <></>
     }
-    console.log(nameInput)
+
+
     return <div id="AddNewPersonContainer">
-        <input placeholder="Name" id="nameInput" onChange={(evt) => { nameInput = evt.target.value }}></input>
-        <input placeholder="email" id="emailInput" type="email" onChange={(evt) => { emailInput = evt.target.value }}></input>
-        <input placeholder="Starting Balance" id="balanceInput" type="number" min="0" step="any" onChange={(evt) => { balanceInput = +evt.target.value }}></input>
+        <input placeholder="Name" id="nameInput" onChange={(evt) => setNameInput(evt.target.value)} onBlur={(evt)=>checkInput()}></input>
+        <input placeholder="email" id="emailInput" type="email" onChange={(evt) => setEmailInput(evt.target.value)} onBlur={(evt)=>checkInput()}></input>
+        <input placeholder="Starting Balance" id="balanceInput" type="number" min="0" step="any" onChange={(evt) => setBalanceInput(+evt.target.value)} onBlur={(evt)=>checkInput()}></input>
         €
-        <button id="submitAddButton" onClick={(evt) => { checkInput() }}>Add</button>
+        <button id="submitAddButton" onClick={(evt) => { console.log(checkInput()) }}>Add</button>
+        <div>{inputHint}</div>
     </div>
 }
