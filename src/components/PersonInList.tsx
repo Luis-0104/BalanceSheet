a606323ef8561501526597aeede61a990576c76e
luis-0104 <schweinberger@milongroup.com>
2022-11-25 11:51:51 +0100

    connected buttons to mst

--- src/components/PersonInList.tsx
+++ src/components/PersonInList.tsx
@@ -1,34 +1,67 @@
 import { useState } from "react";
+import { balanceEmptyError } from "../errors/balanceEmptyError";
 
 type personInListProps = {
-    person: {
-        id: any;
-        name: string;
-        email: string;
-        balance: number;
-    }
-}
+  person: {
+    id: any;
+    name: string;
+    email: string;
+    balance: number;
+    updateBalance: (difference: number) => void;
+    changeEmail: (newEmail: string) => void;
+    remove: () => void;
+  };
+};
 
 export function PersonInList({ person: person }: personInListProps) {
-
-    const [showPersonMenu,setShowPersonMenu] = useState(false)
-
-    function toggleShowPersonMenu(){
-        setShowPersonMenu(!showPersonMenu);
-      }
-    function PersonMenu(){
-        if(!showPersonMenu) return <></>
-        return <div>
-           <button>-1,5€</button>
-           <button>-1€</button>
-        </div>
+  const [showPersonMenu, setShowPersonMenu] = useState(false);
+  const updateBalance = (val: number) => {
+    try {
+      person.updateBalance(val);
+    } catch (error: any) {
+      alert(error.message);
     }
+    toggleShowPersonMenu();
+  };
 
-    return <div>
-        <button onClick={(evt)=>{toggleShowPersonMenu()}}>
-            {person.name + '   -   ' + person.balance + '€'}
+  const toggleShowPersonMenu = () => {
+    setShowPersonMenu(!showPersonMenu);
+  };
+
+  const PersonMenu = () => {
+    if (!showPersonMenu) return <></>;
+    return (
+      <div>
+        <button
+          className="Getränk"
+          onClick={(evt) => {
+            updateBalance(-1.5);
+          }}
+        >
+          -1,5€
         </button>
-        {PersonMenu()}
-        
+        <button
+          className="Getränk"
+          onClick={(evt) => {
+            updateBalance(-1);
+          }}
+        >
+          -1€
+        </button>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <button
+        onClick={(evt) => {
+          toggleShowPersonMenu();
+        }}
+      >
+        {person.name + "   -   " + person.balance + "€"}
+      </button>
+      {PersonMenu()}
     </div>
+  );
 }
--- src/data/dataHandler.ts
+++ src/data/dataHandler.ts
@@ -1,4 +1,3 @@
-var fs = require("fs");
 import savedData from "./data.json";
 type dataProps = {
   personList: {
