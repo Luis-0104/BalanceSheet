a77806decc4cecd46256da9065c80171f26a7be1
luis-0104 <schweinberger@milongroup.com>
2022-12-08 13:09:32 +0100

    removed standard css

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useRootStore } from "../models/Root";
-import "./styling/SearchBar.css";
+
 
 export function SearchBar() {
   const {
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import theme from "./theme";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <CssBaseline />
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
+);
